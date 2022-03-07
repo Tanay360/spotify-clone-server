d924a76a25b2411df37e6eb7390f713207507d35
Tanay360 <tanaygupta270@gmail.com>
2022-03-07 14:45:43 +0530

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,31 @@
+import express from 'express';
+import YoutubeMusicApi from 'youtube-music-api'
+import ytdl from 'ytdl-core';
+const app = express();
+const port =  process.env.PORT || 8000;
+const api = new YoutubeMusicApi();
+await api.initalize()
+
+app.get('/search/:query', async (req, res) => {
+    try {
+        const query = req.params.query as string
+        const result = await api.search(query)
+        res.status(200);
+        res.json(result)
+    } catch (e) {
+        res.status(500)
+        res.end()
+    }
+})
+
+app.get('/download/:id', async (req, res) => {
+    const url = `https://www.youtube.com/watch?v=${req.params.id}`
+    res.header("Content-Disposition", 'attachment;\\  filename="Video.mp4');    
+    ytdl(url, {
+        filter: 'audioonly'
+    }).pipe(res);
+})
+
+app.listen(port, () => {
+  return console.log(`Express is listening at http://localhost:${port}`);
+});
